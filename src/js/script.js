37761814a9e262348840c46f8b71727e09042395
Lcourse <101373417+Lcourse@users.noreply.github.com>
2023-08-15 00:34:37 -0300

    if

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,20 @@
-const inputs = document.querySelectorAll('.input')
+const inputs = document.querySelectorAll(".input");
 
 const handleFocus = ({ target }) => {
-    const span = target.previousElementSibling
-    span.classList.add('span-active')  
-}
+  const span = target.previousElementSibling;
+  span.classList.add("span-active");
+};
 
 const handleFocusout = ({ target }) => {
-    const span = target.previousElementSibling
-    span.classList.remove('span-active')
+  if (target.value == "") {
+    const span = target.previousElementSibling;
+    span.classList.remove("span-active");
+  }
+};
 
-}
-
-
-inputs.forEach((input) => {input.addEventListener('focus', handleFocus)})
-inputs.forEach((input) => {input.addEventListener('focusout', handleFocusout)})
-
+inputs.forEach((input) => {
+  input.addEventListener("focus", handleFocus);
+});
+inputs.forEach((input) => {
+  input.addEventListener("focusout", handleFocusout);
+});
